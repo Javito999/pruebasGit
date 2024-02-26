55903e655a4e8f9cf9ee26be1b5ca84106e6a98b
Javito999 <patxixav@gmail.com>
2024-02-26 12:28:35 +0100

    javascript

--- Ejercicio2JavaScript/Ejercicio7Apuntes/Ejercicio9Apuntes.js
+++ Ejercicio2JavaScript/Ejercicio7Apuntes/Ejercicio9Apuntes.js
@@ -1,5 +1,36 @@
-function color{
+function color(){
+
+    var color = true;
+
+    var array = [];
+
+    let contador = 0;
+      
+
+    do{
+        var respuesta = prompt("Introduzca un color");
+
+        if(respuesta == "rojo"){
+            color = false;
+            contador++;
+
+        }else if(respuesta == "verde"){
+            continue;
+            contador++;
+
+         }else {
+                contador ++;
+                array.push(respuesta)
+                continue;
+         }
 
 
+
+
+    }while(color)
+
+    console.log(array);
+
+    console.log("SE han introducido el color " + contador + " veces");
     
 }
