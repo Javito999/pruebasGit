f4539c578f78599dc4118374d5b4fe18b135a342
Javito999 <patxixav@gmail.com>
2024-03-13 13:34:06 +0100

    examen

---
+++ js_FranciscoJavierGarcia.js
@@ -0,0 +1,107 @@
+/* ejercicio 1 */
+
+function ejercicio1(){
+
+    let numero = prompt("Introduzca un número");
+
+    let personas = [];
+
+    for(i = 0; i <numero; i++){
+
+        let nombre = prompt("Introduzca su nombre");
+
+        console.log(personas[nombre])
+
+
+    }
+        if(numero != )
+
+
+}
+
+
+/* ejercicio 2*/
+
+function ejercicio2(){
+
+    let numero = prompt("Introduzca un número");
+
+    let array = [];
+
+    for(i = 0; i < 10; i++){
+
+        return array[i]/2;
+ }
+
+
+
+}
+
+
+
+/* ejercicio 3*/
+
+// 1º sentencia: pide una palabra (de valor constante) por pantalla
+// 2ª sentencia: pide otra constante y la almacena en la variable palabra2
+// 3ª sentencia: declara la constante palabraMayor, dándole a la función dos
+// valores por parámetro, palabra1 y palabra2.
+// 4ª sentencia: si la longitud de la palabra1 es mayor que la de la palabra2,
+// sale un mensaje por ventana modal diciendo que la palabra2 tiene más letras que
+// la palabra1.
+// 5ª sentencia: en caso de que la longitud de la palabra1 sea menor que la de la 
+// palabra2, sale mensaje por pantalla: lo introducido en la variable palabra2 tiene más
+// letras que las que tiene la palabra1.
+// 6ª sentencia: si no se cumplen ninguna de las dos condiciones anteriores, sale otro mensaje
+// por pantalla que dice ambas variables tienen el mismo número de letras.
+// 7ª sentencia: es la ejecución de la función palabraMayor con esos parámetros.
+
+
+
+/* ejercicio 4 */
+
+function informaUsuario(){
+
+    let funcion = prompt("Introduzca la función que quiera ejecutar");
+
+    switch(funcion){
+
+        case 1:
+
+        ejercicio1();
+
+        console.log("La ejecución ha terminado");
+
+        break;
+
+        case 2:
+
+        ejercicio2();
+
+        if(ejercicio2 == true){
+
+            console.log("Toda ha terminado bien")
+        }
+
+        break;
+
+        case 3:
+
+        console.log("Saliendo de la ejecución");
+
+        break;
+
+
+
+
+
+    }
+
+
+}
+
+/* ejercicio 5 */
+
+// Repasados los ejercicios
+
+
+
